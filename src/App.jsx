212187eef9b4959afc08fb5043b99e7f6894976a
Ross Greenall <rossgreenall@gmail.com>
2021-09-20 23:51:54 +0100

    Added scss styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import './App.css'
+import './App.scss'
 import Navigation from './components/Navigation'
 import About from './pages/About'
 import Homepage from './pages/Homepage'
@@ -9,23 +9,25 @@ import Cart from './pages/Cart'
 
 function App () {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Switch>
-        <Route path='/products'>
-          <Products />
-        </Route>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/cart'>
-          <Cart />
-        </Route>
-        <Route path='/'>
-          <Homepage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <div className='site'>
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route path='/products'>
+            <Products />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/cart'>
+            <Cart />
+          </Route>
+          <Route path='/'>
+            <Homepage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
+import '../styles/homepage.scss'
+import Keycap from '../images/keycap.jpg'
 
-const Homepage = () => <h1>Hi</h1>
+const Homepage = () => {
+  return (
+    <div
+      style={{ background: `url(${Keycap}) no-repeat center center fixed` }}
+      className='home_page'
+    >
+      <div className='landing_page'>
+        <div>
+          <h1 className='page_title'>deskpads.uk</h1>
+          <p>
+            We sell high-quality deskpads, keycaps and keyboard accessories to
+            UK enthusiats.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Homepage
